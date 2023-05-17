dd9200e3d13fd0fd6f94e6f51e26413328d368b1
liji <liji@moolink.com>
2023-05-17 13:19:14 +0800

    add comment

--- websocket/global/global.go
+++ websocket/global/global.go
@@ -31,6 +31,6 @@ type Message struct {
 	FromID   int    `json:"fromID,omitempty" form:"fromID"`     // 发送消息用户ID
 	DestID   int    `json:"destID,omitempty" form:"destID"`     // 接收消息用户ID
 	Msg      string `json:"msg,omitempty" form:"msg"`           // 消息内容
-	MsgType  int    `json:"msgType,omitempty" form:"msgType"`   // 消息自定义类型
+	MsgType  int    `json:"msgType,omitempty" form:"msgType"`   // 消息自定义类型（文本消息/图片消息/语音消息...）
 	AckMsgID int    `json:"ackMsgID,omitempty" form:"ackMsgID"` // 回复消息ID
 }
